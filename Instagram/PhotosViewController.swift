974e5a8daf1833df84ea23e7ee65c7997c245e92
Angel Kou <kouj@purdue.edu>
2016-01-21 16:00:14 -0500

    First commit

---
+++ Instagram/PhotosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Jiayi Kou on 1/21/16.
+//  Copyright © 2016 Jiayi Kou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var data : NSMutableArray = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.data = responseDictionary["data"] as! NSMutableArray
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
